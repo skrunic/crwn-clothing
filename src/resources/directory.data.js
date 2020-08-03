b0387773be36f6e35f20a3439052cc333e9513a7
Siniša <sinisa.krunic@triple-innovations.com>
2020-08-03 21:27:34 +0200

    Add resources folder to store data

--- src/resources/directory.data.js
+++ src/resources/directory.data.js
@@ -1,4 +1,4 @@
-const sections = [
+const SECTIONS_DATA = [
   {
     title: 'hats',
     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
@@ -32,3 +32,5 @@ const sections = [
     linkUrl: 'shop/mens'
   }
 ];
+
+export default SECTIONS_DATA;
