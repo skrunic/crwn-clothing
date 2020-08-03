0d5de1f91e1bb6bd2ecc739ff65680c5975cabdc
Siniša <sinisa.krunic@triple-innovations.com>
2020-08-03 21:18:08 +0200

    Update menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage.component.jsx';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />
+  return (
+    <div>
+      {
+        /**
+         * 'exact' prop forces path to be exact, e.g. path="/" would render localhost:3000/ and localhost:3000/hats pages
+         * 
+         * <Switch> switches views by rendering only the first match
+         */
+      }
+      <Switch>
+        <Route exact={true} path="/" component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage}></Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
