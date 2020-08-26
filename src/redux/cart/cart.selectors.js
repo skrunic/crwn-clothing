ffb89f704b42159059b45ac473fe77ff919c870c
Siniša <skrunic@users.noreply.github.com>
2020-08-26 23:54:37 +0200

    New selectors
    Add user and cart selectors.

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -11,11 +11,23 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems // function to return value we want from the selector; output order mimics the order of input selectors
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);
 
-export const selectCartsItemCount = createSelector(
+export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce(
         (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 
         0
     )
-)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 
+        0
+    )
+);
